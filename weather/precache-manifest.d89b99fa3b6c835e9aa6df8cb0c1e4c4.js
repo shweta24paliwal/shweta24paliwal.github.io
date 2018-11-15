93317e1eb5370f640715eb40033b287f7aeeb621
Shweta <shweta.p1992@gmail.com>
2018-11-15 10:25:44 +0530

    weather widget build added

---
+++ weather/precache-manifest.d89b99fa3b6c835e9aa6df8cb0c1e4c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b4928ae41af3026ae4b2",
+    "url": "/static/js/main.b4928ae4.chunk.js"
+  },
+  {
+    "revision": "af6dee3f8dbf9e3a5aad",
+    "url": "/static/js/1.af6dee3f.chunk.js"
+  },
+  {
+    "revision": "b4928ae41af3026ae4b2",
+    "url": "/static/css/main.3ed66bf5.chunk.css"
+  },
+  {
+    "revision": "ad1834812c6817443c31751602eb03c0",
+    "url": "/index.html"
+  }
+];
