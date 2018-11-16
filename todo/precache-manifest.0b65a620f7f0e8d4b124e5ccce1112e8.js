777dc0fba41ee7b6a3af606c1f400e9093cdb1f0
Shweta <shweta.p1992@gmail.com>
2018-11-16 11:26:00 +0530

    todo add new build

---
+++ todo/precache-manifest.0b65a620f7f0e8d4b124e5ccce1112e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "116863cd5b4dbd9ea319",
+    "url": "/static/js/main.116863cd.chunk.js"
+  },
+  {
+    "revision": "fe23a06f2328183a4b18",
+    "url": "/static/js/1.fe23a06f.chunk.js"
+  },
+  {
+    "revision": "116863cd5b4dbd9ea319",
+    "url": "/static/css/main.635ca7c2.chunk.css"
+  },
+  {
+    "revision": "21f5ec471cb48eef728d6227209e48d1",
+    "url": "/index.html"
+  }
+];
